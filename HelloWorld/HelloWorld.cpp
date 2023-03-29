25b8f433b58867a9bd405890540d833617dcce17
Mateusz Żukowski <mateusz.zukowski.szkola@gmail.com>
2023-03-29 13:09:09 +0200

    Add project files.

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int sgn(double value)
+{
+    return (0 < value) - (value < 0);
+}
+
+int main()
+{
+    for (int x = 0; x < 10; x++)
+    {
+        cout << (char)((sgn(4.5 - x) + 1) / 2 * (1.125 * pow(x, 4) - 9.583 * pow(x, 3) + 25.875 * pow(x, 2) - 20.417 * x + 72) + (sgn(x - 4.5) + 1) / 2 * (1.125 * pow(x, 4) - 32.584 * pow(x, 3) + 347.877 * pow(x, 2) - 1624.427 * x + 2883));
+
+        if (x == 4) cout << ' ';
+        if (x == 9) cout << '!';
+    }
+}
