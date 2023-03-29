727bd79523bfb3964f639d89634e68158f3ebca7
Mateusz Żukowski <mateusz.zukowski.szkola@gmail.com>
2023-03-29 13:27:29 +0200

    small adjustments

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
@@ -14,6 +15,6 @@ int main()
         cout << (char)((sgn(4.5 - x) + 1) / 2 * (1.125 * pow(x, 4) - 9.583 * pow(x, 3) + 25.875 * pow(x, 2) - 20.417 * x + 72) + (sgn(x - 4.5) + 1) / 2 * (1.125 * pow(x, 4) - 32.584 * pow(x, 3) + 347.877 * pow(x, 2) - 1624.427 * x + 2883));
 
         if (x == 4) cout << ' ';
-        if (x == 9) cout << '!';
+        if (x == 9) cout << "!\n";
     }
 }
